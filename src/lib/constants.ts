43594a4fa1244c334a29b1ae0bc19f48d05702bc
Travis Van Nimwegen <github@travis2.com>
2025-04-18 14:15:45 -0400

    Add new application icons and update author information in Cargo.toml

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,10 @@
 
 
 export const HEADER_LINK = 'https://gptree.dev'
+
+export const WEBSITE_LINK = 'https://gptree.dev'
+
+export const GITHUB_LINK = 'https://github.com/travisvn/gptree-gui'
+
+// export const DISCORD_LINK = 'https://discord.gg/gptree'
+
